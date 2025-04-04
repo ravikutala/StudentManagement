197dc2e27045084a22f519bb14daf827687d8eea
ravikutala <ravikutala@gmail.com>
2025-04-04 07:13:05 +0530

    Add files via upload

---
+++ main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2IKK5EUY.mjs";import"./chunk-YG2RK4KR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ student.ts
@@ -0,0 +1,10 @@
+export class Student {
+    studentId : number = 0;
+    studentName : string = "";
+    studentClass :string = "";
+    studentSection :string = "";
+    yearFee : number = 0;
+    feePaid : number = 0;
+    balanceFee : number = 0;
+
+}
